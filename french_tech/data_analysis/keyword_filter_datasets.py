a305c743b551db7bb67decce309bb699f2ebcb6f
thinkcentre <mysqlprojet@gmail.com>
2023-03-06 18:58:46 +0000

    added create keywords in main

--- french_tech/data_analysis/keyword_filter_datasets.py
+++ french_tech/data_analysis/keyword_filter_datasets.py
@@ -26,27 +26,31 @@ data_df: pd.DataFrame = pd.read_csv(filepath_or_buffer=FILE_FULL_PATH,
                                                 COLUMNS_TO_FILTER})
 
 
-# create dataset of non-duplicate choices in column to filter
-def create_sector_set(types_column: list):
-    for sector_type in types_column:
-        filter_keywords.add(sector_type)
+def create_keywords_datasets():
+    # create dataset of non-duplicate choices in column to filter
+    def create_sector_set(types_column: list):
+        for sector_type in types_column:
+            filter_keywords.add(sector_type)
+
+    # loop through columns and create all dataset of keywords
+    for column_to_filter in COLUMNS_TO_FILTER:
+        filter_keywords: set = set()
+
+        # filter all keywords
+        data_df[column_to_filter].apply(lambda x: create_sector_set(x))
+
+        # sort keywords
+        sorted(filter_keywords, reverse=False)
+
+        # save available choices
+        column_choices_df: pd.DataFrame = pd.DataFrame(data=list(filter_keywords), columns=[column_to_filter])
+        save_path = Path(get_project_download_path(), f'column_filter_{column_to_filter}.csv')
+        column_choices_df.to_csv(path_or_buf=save_path,
+                                 sep=',',
+                                 index=False)
+
+        print(f'Found {len(filter_keywords)} unique keywords for column: "{column_to_filter}"')
 
 
-# loop through columns and create all dataset of keywords
-for column_to_filter in COLUMNS_TO_FILTER:
-    filter_keywords: set = set()
-
-    # filter all keywords
-    data_df[column_to_filter].apply(lambda x: create_sector_set(x))
-
-    # sort keywords
-    sorted(filter_keywords, reverse=False)
-
-    # save available choices
-    column_choices_df: pd.DataFrame = pd.DataFrame(data=list(filter_keywords), columns=[column_to_filter])
-    save_path = Path(get_project_download_path(), f'column_filter_{column_to_filter}.csv')
-    column_choices_df.to_csv(path_or_buf=save_path,
-                             sep=',',
-                             index=False)
-
-    print(f'Found {len(filter_keywords)} unique keywords for column: "{column_to_filter}"')
+if __name__ == '__main__':
+    create_keywords_datasets()
--- french_tech/main.py
+++ french_tech/main.py
@@ -1,7 +1,7 @@
 """Launch the webscraper"""
+from french_tech.data_analysis.keyword_filter_datasets import create_keywords_datasets
 from french_tech.scrap_data.ecosystem_webpages.eco_french_tech import get_french_startups_data
-
-get_french_startups_data()
 
 if __name__ == '__main__':
     get_french_startups_data()
+    create_keywords_datasets()
