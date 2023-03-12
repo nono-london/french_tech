72c7c05684f39ac5b6e9a5707d5eeb92f03690e5
thinkcentre <mysqlprojet@gmail.com>
2023-03-12 16:21:32 +0000

    added folder app to the sys packing function

--- french_tech/app_config.py
+++ french_tech/app_config.py
@@ -46,36 +46,41 @@ def get_project_download_path() -> str:
 
 
 def pack_python_libs_in_path():
+    """Adding manually libs that ae needed for the app to work"""
+
     python_app_folder_path: Path = Path(get_project_root_path()).parent
+    if python_app_folder_path.parent not in sys.path:
+        sys.path.insert(1, str(python_app_folder_path.parent))
 
-    mysql_helpers_folder_path: Path = Path(python_app_folder_path, 'mysql_helpers')
-    print(mysql_helpers_folder_path)
-
-    # insert path in 2nd position, first position is reserved
-    if mysql_helpers_folder_path not in sys.path:
-        sys.path.insert(1, str(mysql_helpers_folder_path))
-
-    postgres_folder_path: Path = Path(python_app_folder_path, 'postgresql_helpers')
-    # insert path in 2nd position, first position is reserved
-    if postgres_folder_path not in sys.path:
-        sys.path.insert(1, str(postgres_folder_path))
-
-    proxy_helpers_folder_path: Path = Path(python_app_folder_path, 'proxy_helpers')
-    # insert path in 2nd position, first position is reserved
-    if proxy_helpers_folder_path not in sys.path:
-        sys.path.insert(1, str(proxy_helpers_folder_path))
-
-    selenium_folder_path: Path = Path(python_app_folder_path, 'selenium_helpers')
-    # insert path in 2nd position, first position is reserved
-    if selenium_folder_path not in sys.path:
-        sys.path.insert(1, str(selenium_folder_path))
+    # mysql_helpers_folder_path: Path = Path(python_app_folder_path, 'mysql_helpers')
+    # print(mysql_helpers_folder_path)
+    #
+    # # insert path in 2nd position, first position is reserved
+    # if mysql_helpers_folder_path not in sys.path:
+    #     sys.path.insert(1, str(mysql_helpers_folder_path))
+    #
+    # postgres_folder_path: Path = Path(python_app_folder_path, 'postgresql_helpers')
+    # # insert path in 2nd position, first position is reserved
+    # if postgres_folder_path not in sys.path:
+    #     sys.path.insert(1, str(postgres_folder_path))
+    #
+    # proxy_helpers_folder_path: Path = Path(python_app_folder_path, 'proxy_helpers')
+    # # insert path in 2nd position, first position is reserved
+    # if proxy_helpers_folder_path not in sys.path:
+    #     sys.path.insert(1, str(proxy_helpers_folder_path))
+    #
+    # selenium_folder_path: Path = Path(python_app_folder_path, 'selenium_helpers')
+    # # insert path in 2nd position, first position is reserved
+    # if selenium_folder_path not in sys.path:
+    #     sys.path.insert(1, str(selenium_folder_path))
 
 
 if __name__ == '__main__':
-    print(get_external_ip_address())
-
-    print(get_project_root_path())
-
-    print(get_project_download_path())
+    # print(get_external_ip_address())
+    #
+    # print(get_project_root_path())
+    #
+    # print(get_project_download_path())
 
     pack_python_libs_in_path()
+    print(sys.path)
