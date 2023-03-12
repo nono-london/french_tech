44c06a5527303b49a441e365774199596c1e339e
thinkcentre <mysqlprojet@gmail.com>
2023-03-12 18:53:56 +0000

    deleted web scrap technics unused (bs4 etc)

--- french_tech/scrap_data/ecosystem_webpages/scrap_helpers/scrap_company_data.py
+++
@@ -1,9 +0,0 @@
-# import requests
-from urllib import request
-
-from bs4 import BeautifulSoup as bs
-
-web_request = request.urlopen(url="https://ecosystem.lafrenchtech.com/companies/holiworking")
-web_soup = bs(web_request, 'lxml')
-
-print(web_soup.prettify())
