4eb6b0ef5987f083f725a1e378a95b8bbca48753
hplaptop <mysqlproject@gmail.com>
2023-03-12 18:21:31 +0100

    added lib packer to main.py

--- french_tech/main.py
+++ french_tech/main.py
@@ -1,4 +1,8 @@
 """Launch the webscraper"""
+from app_config import pack_python_libs_in_path
+# pack libs in python sys path
+pack_python_libs_in_path()
+
 from french_tech.data_analysis.keyword_filter_datasets import create_keywords_datasets
 from french_tech.scrap_data.ecosystem_webpages.eco_french_tech import get_french_startups_data
 
