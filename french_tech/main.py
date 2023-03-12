f7f570a1fa6f0b822cf426a12ea09164bca1dcc4
hplaptop <mysqlproject@gmail.com>
2023-03-12 19:11:54 +0100

    main defaults to full data webscrap in headless mode

--- french_tech/main.py
+++ french_tech/main.py
@@ -5,7 +5,8 @@ pack_python_libs_in_path()
 
 from french_tech.data_analysis.keyword_filter_datasets import create_keywords_datasets
 from french_tech.scrap_data.ecosystem_webpages.eco_french_tech import get_french_startups_data
+from french_tech.scrap_data.ecosystem_webpages.eco_french_tech_all import get_french_startups_data
 
 if __name__ == '__main__':
-    get_french_startups_data()
+    get_french_startups_data(headless=True)
     create_keywords_datasets()
