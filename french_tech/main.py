4c7fcf22db2c9bd1cc1ab40d28593e5512e32e2f
HP-ELITE <mysqlproject@gmail.com>
2023-03-13 02:10:36 +0100

    doing both data sets

--- french_tech/main.py
+++ french_tech/main.py
@@ -8,7 +8,13 @@ from french_tech.data_analysis.keyword_filter_datasets import create_keywords_da
 from french_tech.scrap_data.ecosystem_webpages.eco_french_tech import get_french_startups_data
 
 if __name__ == '__main__':
+    print(f"Getting filtered data: expect about 500 companies")
     get_french_startups_data(headless=True,
                              all_data=False,
                              print_errors=False)
+    print(f"Getting UNFILTERED DATA: expect about 10k+ companies")
+    get_french_startups_data(headless=True,
+                             all_data=True,
+                             print_errors=False)
+
     create_keywords_datasets()
