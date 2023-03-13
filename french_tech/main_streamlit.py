a7d9e6fb87bf9c906965bc0d9ba55fa3e7504ab5
hplaptop <mysqlproject@gmail.com>
2023-03-13 23:30:05 +0100

    adding streamlit

--- french_tech/main_streamlit.py
+++ french_tech/main_streamlit.py
@@ -1,31 +1,30 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
+
 from french_tech.data_readers.read_saved_data import (dataset_reader,
                                                       read_markets,
                                                       read_types)
 
 DATA_DF = dataset_reader("2023-03-12_french_startups.csv")
 
-
 st.title("Company Search")
 
 left_col_row1, right_column_row1 = st.columns(2)
 with left_col_row1:
-    selected_markets:list = st.multiselect(label="Company Markets",
-                                  options=read_markets())
+    selected_markets: list = st.multiselect(label="Company Markets",
+                                            options=read_markets())
 with right_column_row1:
-    selected_types:list = st.multiselect(label="Company Types",
-                                options=read_types())
+    selected_types: list = st.multiselect(label="Company Types",
+                                          options=read_types())
 
-def filter_dataset(selected_markets:list, selected_types:list)->pd.DataFrame:
-    temp_df = DATA_DF[pd.DataFrame(DATA_DF['market'].tolist()).isin(selected_markets).any(1).values]
-    temp_df = temp_df[pd.DataFrame(temp_df['type'].tolist()).isin(selected_types).any(1).values]
+
+@st.cache_data
+def filter_dataset(markets: list, types: list) -> pd.DataFrame:
+    temp_df = DATA_DF[pd.DataFrame(DATA_DF['market'].tolist()).isin(markets).any(1).values]
+    temp_df = temp_df[pd.DataFrame(temp_df['type'].tolist()).isin(types).any(1).values]
     return temp_df
 
 
-print(selected_markets)
-print(selected_types)
-#data_df = dataset_reader("2023-03-12_french_startups.csv")
-st.dataframe(filter_dataset(selected_markets=selected_markets,
-                            selected_types=selected_types)
+st.dataframe(filter_dataset(markets=selected_markets,
+                            types=selected_types)
              )
