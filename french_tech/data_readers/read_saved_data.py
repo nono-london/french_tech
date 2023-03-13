4c4402176604712af0ea4ae9aab1ad166b77a7b6
hplaptop <mysqlproject@gmail.com>
2023-03-13 22:26:19 +0100

    changed data reader to avoid bug with NA

--- french_tech/data_readers/read_saved_data.py
+++ french_tech/data_readers/read_saved_data.py
@@ -13,7 +13,8 @@ pd.set_option('display.max_columns', None)
 def dataset_reader(file_name: str):
     # variables
     list_columns: list = ['market', 'type']  # columns with list
-    int_columns: list = ['web_visits_chg_1Y', 'web_employees_chg_1Y', ]  # columns with int
+    int_columns: list = []  # columns with int
+    float_columns: list = ['web_visits_chg_1Y', 'web_employees_chg_1Y', ]  # columns with floats or int that contains NA
     # check if file exists:
     file_full_path: Path = Path(get_project_download_path(), file_name)
     if not file_full_path.exists():
@@ -21,17 +22,19 @@ def dataset_reader(file_name: str):
 
     converters_list: dict = {column_to_filter: literal_eval for column_to_filter in
                              list_columns}
-    converters_int: dict = {column_to_filter: int for column_to_filter in
-                            int_columns}
-    # merge all dicts
-    converters_all: dict = {**converters_list, **converters_int}
 
     data_df: pd.DataFrame = pd.read_csv(filepath_or_buffer=file_full_path,
-                                        converters=converters_all,
+                                        converters=converters_list,
                                         )
+
+    for column in int_columns:
+        data_df[column] = data_df[column].astype(int, errors='ignore')
+    for column in float_columns:
+        data_df[column] = data_df[column].astype(float, errors='ignore')
+
     return data_df
 
 
 if __name__ == '__main__':
-    FILE_NAME: str = "2023-03-02_french_startups.csv"
+    FILE_NAME: str = "2023-03-12_french_startups.csv"
     print(dataset_reader(file_name=FILE_NAME))
